a331c826e302aa947c0ff6c001a0ee6c76252553
IshaHarmalkar <167905999+IshaHarmalkar@users.noreply.github.com>
2026-09-03 14:05:55 +0530

    Debug set to false for app

--- app.py
+++ app.py
@@ -100,4 +100,5 @@ if __name__ == "__main__":
 
 
     #app.run(debug=True, port=5000)
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    #app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run()
